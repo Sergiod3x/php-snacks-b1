c633cd2d9f0b23d399119a77b1a989036f18ec32
Sergiod3x <sergiod3x.@gmail.com>
2021-08-27 04:45:58 +0200

    1-2-3 ok

--- snack1.php
+++ snack1.php
@@ -1,10 +1,3 @@
-<!-- 
-    Passare come parametri GET name, mail e age e verificare 
-    (cercando i metodi che non conosciamo nella documentazione) che
-    name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola
-     e che age sia un numero.
-Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
--->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,6 +7,61 @@ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
     <title>Document</title>
 </head>
 <body>
+<!-- 
+    Creiamo una struttura dati che rappresenta le partite di basket
+    di un’ipotetica tappa del calendario.
+    Ogni array interno conterrà le seguenti informazioni:
+    una squadra di casa
+    una squadra ospite
+    punti fatti dalla squadra di casa
+    punti fatti dalla squadra ospite
+    Stampiamo a schermo tutte le partite con questo schema:
+    Olimpia Milano - Cantù | 55-60
+-->
+
+<?php
+
+    $partite = [
+        [
+            'casa'=>'Olimpia',
+            'ospite'=>'Cantu',
+            'casaPunti'=>'60',
+            'ospitePunti'=>'55'
+
+        ],
+        [
+            'casa'=>'Virtus Roma',
+            'ospite'=>'Virtus Bologna',
+            'casaPunti'=>'40',
+            'ospitePunti'=>'20'
+
+        ],
+        [
+            'casa'=>'F.C Matera',
+            'ospite'=>'Inter',
+            'casaPunti'=>'55',
+            'ospitePunti'=>'1'
+
+        ]
+    ];
+
+while(current($partite)){
+        $kk = key($partite);
+        echo ($partite[$kk]["casa"])." - ".($partite[$kk]["ospite"])." - ".($partite[$kk]["casaPunti"])." - ".($partite[$kk]["ospitePunti"]).'<br />';
+        next($partite);
+}
+
+
+?>
+        
+<hr>
+<!-- 
+    Passare come parametri GET name, mail e age e verificare 
+    (cercando i metodi che non conosciamo nella documentazione) che
+    name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola
+     e che age sia un numero.
+    Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+-->
     <h1>
         <?php 
 
@@ -27,7 +75,7 @@ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
                 echo "Accesso negato";
             }
 
-            Var_dump(strlen($name));
+            // Var_dump(strlen($name)) ;
 
 
 
@@ -56,60 +104,77 @@ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
 
     </h1>
 
-    <hr>
 
+    <hr>
+<!-- 
+    Creare un array di array.
+    Ogni array figlio avrà come chiave una data in questo formato:
+    DD/MM/YYYY (ad es 31/01/2007) e come valore un array di post associati
+    a quella data.
+    Stampare ogni data con i relativi post.
+-->
 
 <?php
 
-$posts = [
-
-    '10/01/2019' => [
-        [
-            'title' => 'Post 1',
-            'author' => 'Michele Papagni',
-            'text' => 'Testo post 1'
+    $posts = [
+    
+        '10/01/2019' => [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
         ],
-        [
-            'title' => 'Post 2',
-            'author' => 'Michele Papagni',
-            'text' => 'Testo post 2'
-        ],
-    ],
-    '10/02/2019' => [
-        [
-            'title' => 'Post 3',
-            'author' => 'Michele Papagni',
-            'text' => 'Testo post 3'
-        ]
-    ],
-    '15/05/2019' => [
-        [
-            'title' => 'Post 4',
-            'author' => 'Michele Papagni',
-            'text' => 'Testo post 4'
-        ],
-        [
-            'title' => 'Post 5',
-            'author' => 'Michele Papagni',
-            'text' => 'Testo post 5'
-        ],
-        [
-            'title' => 'Post 6',
-            'author' => 'Michele Papagni',
-            'text' => 'Testo post 6'
-        ]
-    ],
-];
-
-
-foreach ($posts as $v1) {
-    foreach ($v1 as $v2) {
-        echo key($v1);
+        '10/02/2019' => [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+                ]
+            ],
+            '15/05/2019' => [
+                [
+                    'title' => 'Post 4',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 4'
+                ],
+                [
+                    'title' => 'Post 5',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 5'
+                ],
+                [
+                    'title' => 'Post 6',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 6'
+                    ]
+                ],
+            ];
         
-     
-}
+        
+    while (current($posts)){
+        $k = key($posts);
+        echo $k.'<br />';
+        while(current($posts[$k])){
+            $k2 =  key($posts[$k]);
+            while(current($posts[$k][$k2])){
+                echo key($posts[$k][$k2]).'<br />';
+                echo current($posts[$k][$k2]).'<br />';
+                    next($posts[$k][$k2]);
+            }
+            next($posts[$k]);
+        }
+        next($posts);
+    }
+
 ?>
     
+<hr>
     
 </body>
 </html>
